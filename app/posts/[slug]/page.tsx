a5c65a26711ba791796f0f9b5879a498df0cecc9
Martin Ferreira <martin.f.yic@gmail.com>
2023-09-11 21:19:09 -0300

    feat: :lipstick: add styles and author image

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,20 @@
 import { getPostBySlug, markdownToHtml } from '@/lib';
 import markdownStyles from './markdown-styles.module.css';
+import { NextPage } from 'next';
+import Image from 'next/image';
 
-export default async function Post({ params }: { params: { slug: string } }) {
-	const post = getPostBySlug(params.slug, ['title', 'author', 'content']);
+interface Props {
+	params: { slug: string };
+}
+const PostPage: NextPage<Props> = async ({ params }) => {
+	const post = getPostBySlug(params.slug, [
+		'title',
+		'author',
+		'authorImage',
+		'content',
+		'coverImage',
+		'topic',
+	]);
 
 	const content = await markdownToHtml(post.content || '');
 
@@ -10,8 +22,29 @@ export default async function Post({ params }: { params: { slug: string } }) {
 		<div className='w-3/4 mx-auto'>
 			<main>
 				<div className='w-full h-16  dark:text-white'>
+					<picture className='grid place-content-center mb-14'>
+						<Image
+							src={post.coverImage}
+							alt={post.topic}
+							priority
+							width={400}
+							height={400}
+							style={{ width: '100%' }}
+						/>
+					</picture>
 					<h1 className='text-4xl font-semibold'>{post.title}</h1>
-					<p className='text-gray-400'>{post.author}</p>
+					<div className='flex flex-row justify-end items-end gap-2 my-4'>
+						<p className='text-gray-400'>{post.author}</p>
+						<Image
+							className='rounded-full shadow'
+							src={post.authorImage}
+							alt={post.author}
+							priority
+							width={50}
+							height={40}
+						/>
+					</div>
+					<hr />
 					<div
 						className={markdownStyles['markdown']}
 						dangerouslySetInnerHTML={{ __html: content }}
@@ -24,4 +57,6 @@ export default async function Post({ params }: { params: { slug: string } }) {
 			/>
 		</div>
 	);
-}
+};
+
+export default PostPage;
